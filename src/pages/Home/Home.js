83615ace12bb7366306b64b5d89eacc5abba4b23
MartinOisRod <martin.ois.rookie@gmail.com>
2024-12-04 09:28:01 +0100

    Añadir proyectos al home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -73,7 +73,7 @@ const Home = () => {
       </main>
 
       <Empleados />
-      <Eventos />
+      <Proyectos limite={3} />
 
       <section className="nature-section">
         <h2 className="nature-title">
