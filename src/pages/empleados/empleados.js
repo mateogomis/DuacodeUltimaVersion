6b29b917aff9da0a4bc362823728c355fe633254
MartinOisRod <martin.ois.rookie@gmail.com>
2024-12-09 16:04:24 +0100

    arreglo menor empleados

--- src/pages/empleados/empleados.js
+++ src/pages/empleados/empleados.js
@@ -20,7 +20,6 @@ const Empleados = () => {
           "http://localhost:8000/api/empleados/"
         );
         setEmpleados(response.data);
-        console.log(response.data);
         setFilteredEmpleados(response.data);
       } catch (error) {
         setError("Error al obtener los empleados");
@@ -82,90 +81,96 @@ const Empleados = () => {
         onChange={handleSearch}
         className="employee-search-bar"
       />
-      <Slider {...carouselSettings} className="employee-carousel">
-        {filteredEmpleados.map((empleado) => (
-          <div className="employee-card" key={empleado.id}>
-            <div className="employee-image-container">
-              <img
-                src={`http://localhost:8000/media/${empleado.foto}`}
-                alt={empleado.nombre}
-                className="employee-image"
-              />
-            </div>
-            <h3 className="employee-name">{empleado.nombre}</h3>
-            <button
-              className="expand-button"
-              onClick={() => toggleInfo(empleado.id)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-person-plus-fill"
-                viewBox="0 0 16 16"
-              >
-                <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-                <path
-                  fillRule="evenodd"
-                  d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"
+      {filteredEmpleados.length === 0 ? (
+        <p className="no-employees-message">No existe ese empleado.</p>
+      ) : (
+        <Slider {...carouselSettings} className="employee-carousel">
+          {filteredEmpleados.map((empleado) => (
+            <div className="employee-card" key={empleado.id}>
+              <div className="employee-image-container">
+                <img
+                  src={`http://localhost:8000/media/${empleado.foto}`}
+                  alt={empleado.nombre}
+                  className="employee-image"
                 />
-              </svg>
-            </button>
-            {expandedCards[empleado.id] && (
-              <div className="employee-info-expanded">
-                <p>
-                  <strong >Estado: </strong>
-                  {(() => {
-                    if (
-                      empleado.baja === false &&
-                      empleado.excedencia === false &&
-                      empleado.vacaciones === false &&
-                      empleado.teletrabajo === false
-                    ) {
-                      return "Está trabajando";
-                    } else if (empleado.teletrabajo === true) {
-                      return "Está teletrabajando";
-                    } else {
-                      return "No está trabajando";
-                    }
-                  })()}
-                </p>
-                <p>
-                  <strong>Cumpleaños:</strong> {empleado.cumpleanos}
-                </p>
-                <p>
-                  <strong>Contratación:</strong> {empleado.fecha_contratacion}
-                </p>
-                <p>
-                  <strong>Correo:</strong> {empleado.email}
-                </p>
-                <p>
-                  <strong>Teléfono:</strong> {empleado.telefono}
-                </p>
-                <p>
-                  <strong>Oficina:</strong> {empleado.sede.nombre || "No asignada"}
-                </p>
-                <p>
-                  <strong>Dirección:</strong> {empleado.sede.direccion || "No disponible"}
-                </p>
-                <p>
-                  <strong>Supervisor:</strong> {empleado.supervisor}
-                </p>
-                <p>
-                  <strong>Puesto:</strong> {empleado.rol.rol_display}
-                </p>
-                <p>
-                  <strong>Vacaciones: </strong>
-                  {empleado.vacaciones
-                    ? "Está de vacaciones"
-                    : "No está de vacaciones"}
-                </p>
               </div>
-            )}
-          </div>
-        ))}
-      </Slider>
+              <h3 className="employee-name">
+                {empleado.nombre} {empleado.apellido_1} {empleado.apellido_2}
+              </h3>
+              <button
+                className="expand-button"
+                onClick={() => toggleInfo(empleado.id)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                  <path
+                    fillRule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"
+                  />
+                </svg>
+              </button>
+              {expandedCards[empleado.id] && (
+                <div className="employee-info-expanded">
+                  <p>
+                    <strong>Estado: </strong>
+                    {(() => {
+                      if (
+                        empleado.baja === false &&
+                        empleado.excedencia === false &&
+                        empleado.vacaciones === false &&
+                        empleado.teletrabajo === false
+                      ) {
+                        return "Está trabajando";
+                      } else if (empleado.teletrabajo === true) {
+                        return "Está teletrabajando";
+                      } else {
+                        return "No está trabajando";
+                      }
+                    })()}
+                  </p>
+                  <p>
+                    <strong>Cumpleaños:</strong> {empleado.cumpleanos}
+                  </p>
+                  <p>
+                    <strong>Contratación:</strong> {empleado.fecha_contratacion}
+                  </p>
+                  <p>
+                    <strong>Correo:</strong> {empleado.email}
+                  </p>
+                  <p>
+                    <strong>Teléfono:</strong> {empleado.telefono}
+                  </p>
+                  <p>
+                    <strong>Oficina:</strong> {empleado.sede.nombre || "No asignada"}
+                  </p>
+                  <p>
+                    <strong>Dirección:</strong> {empleado.sede.direccion || "No disponible"}
+                  </p>
+                  <p>
+                    <strong>Supervisor:</strong> {empleado.supervisor}
+                  </p>
+                  <p>
+                    <strong>Puesto:</strong> {empleado.rol.rol_display}
+                  </p>
+                  <p>
+                    <strong>Vacaciones: </strong>
+                    {empleado.vacaciones
+                      ? "Está de vacaciones"
+                      : "No está de vacaciones"}
+                  </p>
+                </div>
+              )}
+            </div>
+          ))}
+        </Slider>
+      )}
     </section>
   );
 };
