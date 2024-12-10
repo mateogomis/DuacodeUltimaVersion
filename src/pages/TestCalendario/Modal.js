b8ef198c5163c0e0b4a6239fa610396922341312
mateogomis <mateoopg01@gmail.com>
2024-12-10 11:24:28 +0100

    Funcionalidad Modal Calendario 1

--- src/pages/TestCalendario/Modal.js
+++ src/pages/TestCalendario/Modal.js
@@ -4,8 +4,15 @@ import './Modal.css';
 const Modal = ({ isVisible, onClose, children }) => {
   if (!isVisible) return null;
 
+  // Manejar clics fuera del contenido del modal
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains('custom-modal')) {
+      onClose();
+    }
+  };
+
   return (
-    <div className="custom-modal">
+    <div className="custom-modal" onClick={handleOutsideClick}>
       <div className="custom-modal-content">
         <span className="custom-close" onClick={onClose}>
           &times;
