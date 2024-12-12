8f070ed7b2cd2f45ab57d9af7b0d08ec49fbf156
mateogomis <mateoopg01@gmail.com>
2024-12-12 12:56:28 +0100

    Agregando cambios  error 404

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './NotFound.css'; // Opcional para estilos
+import './NotFound.css'; // Asegúrate de tener los estilos
+import logo from '../assets/images/logo.png'; // Importa la imagen
 
 const NotFound = () => {
   return (
     <div className="not-found">
+      <img src={logo} alt="Logo" className="rotating-logo" />
       <h1>404</h1>
       <p>Lo sentimos, la página que buscas no existe.</p>
       <Link to="/">Volver al inicio</Link>
